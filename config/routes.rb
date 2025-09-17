c2e666b9e70b78b47a06641bb5b5e25ea6335bd3
khin-alu <khin@aluminum.io>
2025-09-17 21:23:29 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "static_pages/home"
   get "static_pages/help"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "static_pages/about"
+  root "application#hello"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "application#hello"
+  # root "posts#index"
 end
